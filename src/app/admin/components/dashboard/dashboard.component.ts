d419922741c76bdf345f4aea1c390e51448ece2f
Karina Nóbrega <nobregakarina7@gmail.com>
2024-09-21 22:36:41 -0300

    dashboard

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../service/admin.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +10,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  products: any[] = [];
+  searchProductForm!: FormGroup;
+
+  constructor(
+    private adminService: AdminService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+    ) { }
+
+  ngOnInit() {
+    this.getAllProducts();
+    this.searchProductForm = this.fb.group({
+      title: [null, [Validators.required]]
+    })
+  }
+
+  getAllProducts() {
+    this.products = [];
+    this.adminService.getAllProducts().subscribe(res => {
+      res.forEach(element => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+      console.log(this.products)
+    })
+  }
+
+  submitForm() {
+    this.products = [];
+    const title = this.searchProductForm.get('title')!.value;
+    this.adminService.getAllProductByName(title).subscribe(res => {
+      res.forEach(element => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+      console.log(this.products)
+    })
+  }
+
+  deleteProduct(productId: any) {
+    this.adminService.deleteProduct(productId).subscribe(res => {
+      if (res.body == null) {
+        this.snackBar.open('Produto Excluido com Sucesso!', 'X', {
+          duration: 5000
+        });
+        this.getAllProducts();
+      } else {
+        this.snackBar.open(res.message, 'Fechar', {
+          duration: 5000,
+          panelClass: 'error-snackBar'
+        });
+      }
+    })
+  }
 
 }
